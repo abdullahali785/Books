0657b2ecd7dbbe0aa31635d24c0a027c4c58b454
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-09-14 20:24:15 -0500

    database connection

---
+++ index.js
@@ -0,0 +1,24 @@
+import bodyParser from "body-parser";
+import express from "express";
+import axios from "axios";
+import fs from "fs";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: fs.readFileSync("password.txt", "utf8"),
+  port: 5432,
+});
+//db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`)
+});
