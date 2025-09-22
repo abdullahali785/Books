1e9fa0c57dfaf9ecd8034fba4649f4e65c10d6dd
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-09-22 17:07:38 -0500

    Increased security

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "books",
-  password: "abdullah2006ali",
+  password: fs.readFileSync("password.txt", "utf8"),
   port: 5432,
 });
 db.connect();
