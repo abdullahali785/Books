de59dae49f9c43720a2c4aba37e4fdef1d0768f6
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-10-13 13:13:05 -0500

    Security

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "books",
-  password: fs.readFileSync("password.txt", "utf8") || process.env.KEY,
+  password: process.env.KEY,
   port: 5432,
 });
 db.connect();
